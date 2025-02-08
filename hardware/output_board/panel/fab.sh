48500a29653c5428e71ec0d27967d2e53623051a
djowel <djowel@gmail.com>
2025-02-08 13:57:19 +0800

    JLCPCB fab details

--- hardware/output_board/panel/fab.sh
+++ hardware/output_board/panel/fab.sh
@@ -6,7 +6,7 @@ kikit panelize \
     --framing 'railstb; width: 5mm; space: 3mm;' \
     --tooling '3hole; hoffset: 2.5mm; voffset: 2.5mm; size: 1.5mm' \
     --fiducials '3fid; hoffset: 5mm; voffset: 2.5mm; coppersize: 2mm; opening: 2mm;' \
-    --text 'simple; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
+    --text 'simple; text: JLCJLCJLCJLC; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
     ../output_board.kicad_pcb output_board_panel.kicad_pcb
 
 kikit fab jlcpcb --no-drc output_board_panel.kicad_pcb .
