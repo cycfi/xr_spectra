aa1ef928b3ac4e012c5f2c7227927c3e8ee3d2c7
djowel <djowel@gmail.com>
2025-04-17 11:42:19 +0800

    complete top and bottom coil covers

--- hardware/coil_cover/coil_cover_top_60/panel/fab.sh
+++ hardware/coil_cover/coil_cover_top_60/panel/fab.sh
@@ -1,15 +1,14 @@
 kikit panelize \
-    --layout 'grid; rows: 8; cols: 1; space: 2mm' \
-    --tabs 'fixed; vwidth: 6mm; vcount: 3' \
-    --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: -0.251mm; prolong: -0.3mm' \
+    --layout 'grid; rows: 4; cols: 1; space: 2mm' \
+    --tabs 'fixed; vwidth: 10mm; vcount: 3' \
+    --cuts vcuts \
     --source 'tolerance: 10mm' \
     --post 'millradius: 1mm' \
     --framing 'railstb; width: 5mm; space: 3mm;' \
     --tooling '3hole; hoffset: 2.5mm; voffset: 2.5mm; size: 1.5mm' \
-    --fiducials '3fid; hoffset: 5mm; voffset: 2.5mm; coppersize: 2mm; opening: 1mm;' \
-    --text 'simple; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
-    --post 'millradius: 1mm' \
-    ../bobbin_top_3.3.kicad_pcb  bobbin_top_3.3_panel.kicad_pcb
+    --fiducials '3fid; hoffset: 5mm; voffset: 2.5mm; coppersize: 2mm; opening: 2mm;' \
+    --text 'simple; text: JLCJLCJLCJLC; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
+    ../coil_cover_top_60.kicad_pcb  coil_cover_top_60_panel.kicad_pcb
 
-kikit fab jlcpcb --no-drc bobbin_top_3.3_panel.kicad_pcb .
-mv gerbers.zip bobbin_top_3.3_gerbers.zip
+kikit fab jlcpcb --no-drc coil_cover_top_60_panel.kicad_pcb .
+mv gerbers.zip coil_cover_top_60_gerbers.zip
--- hardware/coil_cover/coil_cover_top_82/panel/fab.sh
+++ hardware/coil_cover/coil_cover_top_82/panel/fab.sh
@@ -1,14 +1,13 @@
 kikit panelize \
-    --layout 'grid; rows: 8; cols: 1; space: 2mm' \
-    --tabs 'fixed; vwidth: 6mm; vcount: 3' \
-    --cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: -0.251mm; prolong: -0.3mm' \
+    --layout 'grid; rows: 4; cols: 1; space: 2mm' \
+    --tabs 'fixed; vwidth: 10mm; vcount: 3' \
+    --cuts vcuts \
     --source 'tolerance: 10mm' \
     --post 'millradius: 1mm' \
     --framing 'railstb; width: 5mm; space: 3mm;' \
     --tooling '3hole; hoffset: 2.5mm; voffset: 2.5mm; size: 1.5mm' \
-    --fiducials '3fid; hoffset: 5mm; voffset: 2.5mm; coppersize: 2mm; opening: 1mm;' \
-    --text 'simple; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
-    --post 'millradius: 1mm' \
+    --fiducials '3fid; hoffset: 5mm; voffset: 2.5mm; coppersize: 2mm; opening: 2mm;' \
+    --text 'simple; text: JLCJLCJLCJLC; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
     ../coil_cover_top_82.kicad_pcb coil_cover_top_82_panel.kicad_pcb
 
 kikit fab jlcpcb --no-drc coil_cover_top_82_panel.kicad_pcb .
